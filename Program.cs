1af34de25b1275d0516e3747bd8425ccaeff0491
Ulisses Cardoso <rasracapur@outlook.com>
2022-06-15 17:56:20 -0300

    removed Implicit usings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Threading.Tasks;
+
 Console.WriteLine("Hello, World!");
